932adb4d30f649664d1ab15c66b57395ebdaeaa4
qiuyue1993 <qiuyue@outlook.jp>
2020-03-04 09:57:40 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,7 +1,9 @@
 import torch
 
+# L1 distance
 total += torch.sum(torch.sum(torch.sum(abs(x_b-x_a), 0), 0), 0)/(64.0*3.0*3.0)
 
+# L2 distance
 total += math.sqrt(torch.sum(torch.sum(pow(x_b-x_a, 2.0), 0), 0), 0)/(64.0*3.0*3.0)
 
 
